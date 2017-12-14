0cd48e150c4910019b77e33951aec2eb681dd053
Brent Scarbrough <BrentScarbrough@brents-mbp-2.attlocal.net>
2017-12-14 15:20:35 -0500

    12.14 Creating Movie Pictures

--- Movie Collector/Movie Collector/MovieCollectorViewController.swift
+++ Movie Collector/Movie Collector/MovieCollectorViewController.swift
@@ -27,9 +27,30 @@ class MovieCollectorViewController:  UIViewController, UIImagePickerControllerDe
         present(imagePicker, animated: true, completion: nil)
     }
     
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        imageBox.image = image
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    
     @IBAction func cameraTapped(_ sender: Any) {
     }
     @IBAction func addImageTapped(_ sender: Any) {
+        
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        let movie = Movie(context: context)
+        movie.title = titleTextField.text
+        movie.image = UIImagePNGRepresentation(imageBox.image!)
+        
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        
+        navigationController!.popViewController(animated: true)
+        
+        
     }
     
 }
--- Movie Collector/Movie Collector/ViewController.swift
+++ Movie Collector/Movie Collector/ViewController.swift
@@ -8,16 +8,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var movies : [Movie] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+         let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        do {
+       movies = try context.fetch(Movie.fetchRequest())
+            tableView.reloadData()
+        } catch {
+            
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let movie = movies[indexPath.row]
+        cell.textLabel?.text = movie.title
+        cell.imageView?.image = UIImage(data: movie.image as! Data)
+        return cell
+        
+
+        
     }
 
 
